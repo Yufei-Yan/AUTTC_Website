d77266e334df18a7e619c8666051888a7a0c9eb1
tangchengyu <chengyutang@outlook.com>
2017-04-29 21:20:31 -0500

    testBlog.xml corrected; added method to class Blog and Comment

--- src/java/com/auttc/business/Blog.java
+++ src/java/com/auttc/business/Blog.java
@@ -10,5 +10,53 @@ package com.auttc.business;
  * @author yufeiyan
  */
 public class Blog {
+    private String title;
+    private String date;
+    private String body;
+    private Comment[] commentList;
     
+    public Blog() {
+        this.title = "";
+        this.date = "";
+        this.body = "";
+    }
+    
+    public Blog(String title, String date, String body, Comment[] commentList) {
+        this.title = title;
+        this.date = date;
+        this.body = body;
+        this.commentList = commentList;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    public String getBody() {
+        return body;
+    }
+    
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+    public Comment[] getComments() {
+        return commentList;
+    }
+    
+    public void setComments(Comment[] commentList) {
+        this.commentList = commentList;
+    }
 }
--- src/java/com/auttc/business/Comment.java
+++ src/java/com/auttc/business/Comment.java
@@ -10,5 +10,43 @@ package com.auttc.business;
  * @author yufeiyan
  */
 public class Comment {
+    private String username;
+    private String cdate;
+    private String cbody;
     
+    public Comment() {
+        this.username = "";
+        this.cdate = "";
+        this.cbody = "";
+    }
+    
+    public Comment(String username, String cdate, String cbody) {
+        this.username = username;
+        this.cdate = cdate;
+        this.cbody = cbody;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getCdate() {
+        return cdate;
+    }
+    
+    public void setCdate(String cdate) {
+        this.cdate = cdate;
+    }
+    
+    public String getCbody() {
+        return cbody;
+    }
+    
+    public void setCbody(String cbody) {
+        this.cbody = cbody;
+    }
 }
